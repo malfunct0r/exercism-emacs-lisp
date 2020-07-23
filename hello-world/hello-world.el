30acc31d251671555fa818f1651cf9a473c7b58a
malfunct0r <malfunct0r@gmail.com>
2020-07-22 23:44:44 -0700

    Add hello-world

---
+++ hello-world/hello-world.el
@@ -0,0 +1,11 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+;; A function that prints "Hello World!"
+;;; Code:
+(defun hello ()
+  "A function that prints \"Hello World!\"."
+  (message "Hello, World!"))
+
+(provide 'hello-world)
+;;; hello-world.el ends here
